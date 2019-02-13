e9a8511fccc5638fc0f9e06e091db6f85a06b95a
oliverochman <oliver.ochman@hotmail.com>
2019-02-13 16:04:09 +0100

    make second test go green

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -3,7 +3,7 @@ Given("I am on the landing page") do
 end
   
   Given("I click on the {string} button") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+    click_on(string)
   end
   
   Then("I fill in {string} with my {string}") do |string, string2|
@@ -13,10 +13,7 @@ end
   Then("I will in {string} with my {string}") do |string, string2|
     pending # Write code here that turns the phrase above into concrete actions
   end
-  
-  Then("I click on the {string} button") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+
   
   Then("I should see {string}") do |string|
     pending # Write code here that turns the phrase above into concrete actions
