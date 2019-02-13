7a480e106044c801fcf36a490e5e5fd710702ec3
faraznaeem <faraznaeem@gmail.com>
2019-02-13 15:57:19 +0100

    add pending step

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,23 @@
+Given("I am on the landing page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given("I click on the {string} button") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I fill in {string} with my {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I will in {string} with my {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I click on the {string} button") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
